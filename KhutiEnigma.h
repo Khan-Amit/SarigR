5e6265df3bc70c8af004c3a10a22e8f59ddc2c27
ENKI <amit.khanna.1082@gmail.com>
2026-06-29 12:50:21 +0700

    Remove comments from encrypt and decrypt methods

--- KhutiEnigma.h
+++ KhutiEnigma.h
@@ -10,11 +10,8 @@ private:
     void initRotors(const std::string& password);
 
 public:
-    // Encrypts plaintext -> Enigma-rotor scramble -> AES-256-GCM
     std::vector<unsigned char> encrypt(const std::vector<unsigned char>& plaintext,
                                        const std::string& password);
-
-    // Decrypts: AES-GCM -> inverse rotor scramble -> plaintext
     std::vector<unsigned char> decrypt(const std::vector<unsigned char>& ciphertext,
                                        const std::string& password);
 };
